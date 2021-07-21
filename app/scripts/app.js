089ffbea931a7ec22198e8c40df8fc654f32ee23
Mark Chen <dsmarkchen@gmail.com>
2021-07-20 23:04:28 -0600

    remove ununsed style files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -65,8 +65,7 @@ angular
            });
     };
 
-
- })
+})
 .directive('xwindow', ['$window', function ($window) {
      return {
         link: link,
@@ -120,43 +119,7 @@ angular
       };
       scope.updateTooltipPosition = function(top, left) {
          var target  = $( this );
-/*         var pos_left = target.offset().left + (target.outerWidth()/2); */
 
-/*         if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-                tooltip.css( 'max-width', $( window ).width() / 2 );
-         else
-                tooltip.css( 'max-width', 340 );
- 
-        var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-                pos_top  = target.offset().top - tooltip.outerHeight() - 20;
- 
-            if( pos_left < 0 )
-            {
-                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-                tooltip.addClass( 'left' );
-            }
-            else
-                tooltip.removeClass( 'left' );
- 
-            if( pos_left + tooltip.outerWidth() > $( window ).width() )
-            {
-                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-                tooltip.addClass( 'right' );
-            }
-            else
-                tooltip.removeClass( 'right' );
- 
-            if( pos_top < 0 )
-            {
-                var pos_top  = target.offset().top + target.outerHeight();
-                tooltip.addClass( 'top' );
-            }
-            else
-                tooltip.removeClass( 'top' );
- 
-            tooltip.css( { left: pos_left, top: pos_top } )
-                   .animate( { top: '+=10', opacity: 1 }, 50 );
-*/
         tooltip.css({
             top: top + 'px',
             left: left + 'px',
